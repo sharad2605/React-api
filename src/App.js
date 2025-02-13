3324092e00ab5fed714a35d7dcc38bb061eb6c56
sharad2605 <sharad26shukla@gmail.com>
2025-02-14 00:52:35 +0530

    Loading application commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import { useState } from 'react';
 function App() {
   
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   
   async function fetchMoviesHandler() {
+    setIsLoading(true);
+
     const response = await fetch('https://swapi.dev/api/films')
      const  data = await response.json();
 
@@ -21,6 +24,7 @@ function App() {
           };
         });
         setMovies(transformedMovies);
+        setIsLoading(false);
       };
   
 
@@ -30,7 +34,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No movies found.Click to fetch</p>}
       </section>
     </React.Fragment>
   );
